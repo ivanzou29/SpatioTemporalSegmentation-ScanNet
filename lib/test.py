eafcb930074932ba97ecbfd5c1600084b7122213
Yunfan Zou <ivanrz29@connect.hku.hk>
2022-11-16 22:38:36 +0800

    eval on dense pointcloud vox-devox

--- lib/test.py
+++ lib/test.py
@@ -6,6 +6,7 @@
 import logging
 import warnings
 
+import open3d as o3d
 import numpy as np
 import torch
 import os
@@ -21,6 +22,31 @@ from lib.datasets.scannet import VALID_CLASS_IDS_200
 import MinkowskiEngine as ME
 
 import wandb
+
+# Needs to be updated when doing evaluation
+TESTING_FILES_TXT = '/local/home/yunzou/scannet_data/scannet_200_processed/train/scannetv2_test.txt'
+TESTING_SCANS_ROOT = '/local/home/yunzou/scannet_data/scannet_200_processed/test'
+PRED_ROOT = 'testing_set_prediction'
+
+# Load point cloud file, from https://github.com/NVIDIA/MinkowskiEngine/blob/master/examples/indoor.py
+def load_file(file_name):
+  pcd = o3d.io.read_point_cloud(file_name)
+  coords = np.array(pcd.points)
+  colors = np.array(pcd.colors)
+  return coords, colors, pcd
+
+# Normalize color function, from https://github.com/NVIDIA/MinkowskiEngine/blob/master/examples/indoor.py
+def normalize_color(color: torch.Tensor, is_color_in_range_0_255: bool = False) -> torch.Tensor:
+    r"""
+    Convert color in range [0, 1] to [-0.5, 0.5]. If the color is in range [0,
+    255], use the argument `is_color_in_range_0_255=True`.
+    `color` (torch.Tensor): Nx3 color feature matrix
+    `is_color_in_range_0_255` (bool): If the color is in range [0, 255] not [0, 1], normalize the color to [0, 1].
+    """
+    if is_color_in_range_0_255:
+        color /= 255
+    color -= 0.5
+    return color.float()
 
 
 def print_info(iteration,
@@ -105,89 +131,44 @@ def test(curr_train_iter, model, data_loader, config, transform_data_fn=None, ha
   # Clear cache (when run in val mode, cleanup training cache)
   torch.cuda.empty_cache()
 
-  pred_res_list = []
 
-  with torch.no_grad():
-    for iteration in range(max_iter):
-      data_timer.tic()
-      if config.return_transformation:
-        coords, input, target, pointcloud, transformation = data_iter.next()
-      else:
-        coords, input, target = data_iter.next()
-      data_time = data_timer.toc(False)
 
-      # Preprocess input
-      iter_timer.tic()
-
-      if config.normalize_color:
-        input[:, :3] = input[:, :3] / 255. - 0.5
-      sinput = ME.SparseTensor(input, coords, device=device) #.to(device)
-
-      # Feed forward
-      inputs = (sinput,)
-      soutput = model(*inputs)
-      output = soutput.F
-      
-      pred = get_prediction(dataset, output, target).int()
-      iter_time = iter_timer.toc(False)
-
-      print(pred.size())
-      print(torch.max(pred))
-      print(torch.min(pred))
-      print(torch.unique(pred))
-      print(len(torch.unique(pred)))
-
-      if has_gt:
-        target_np = target.numpy()
-        num_sample = target_np.shape[0]
-        target = target.to(device)
-
-        cross_ent = criterion(output, target.long())
-        losses.update(float(cross_ent), num_sample)
-        scores.update(precision_at_one(pred, target), num_sample)
-        hist += fast_hist(pred.cpu().numpy().flatten(), target_np.flatten(), num_labels)
-        ious = per_class_iu(hist) * 100
-
-        prob = torch.nn.functional.softmax(output, dim=1)
-        ap = average_precision(prob.cpu().detach().numpy(), target_np)
-        aps = np.vstack((aps, ap))
-        # Due to heavy bias in class, there exists class with no test label at all
-        with warnings.catch_warnings():
-          warnings.simplefilter("ignore", category=RuntimeWarning)
-          ap_class = np.nanmean(aps, 0) * 100.
-      else:
-        pred = [VALID_CLASS_IDS_200[idx] for idx in pred.tolist()]
-        pred_res_list.append(pred)
-        print(len(pred_res_list))
-      # if iteration == max_iter - 1 and iteration > 0:
-      #   reordered_ious = dataset.reorder_result(ious)
-      #   reordered_ap_class = dataset.reorder_result(ap_class)
-      #   class_names = dataset.get_classnames()
-      #   print_info(
-      #       iteration,
-      #       max_iter_unique,
-      #       data_time,
-      #       iter_time,
-      #       has_gt,
-      #       losses,
-      #       scores,
-      #       reordered_ious,
-      #       hist,
-      #       reordered_ap_class,
-      #       class_names=class_names,
-      #       data_type=data_type)
-      if iteration % config.empty_cache_freq == 0:
-        # Clear cache
-        torch.cuda.empty_cache()
-
-  if pred_res_list:
+  # testing mode => output the prediction, original point cloud is needed
+  if data_type == 'testing':
+    pred_res_list = []
     # write the prediction result of test set
     scene_names = []
-    with open('/local/home/yunzou/scannet_data/scannet_200_processed/train/scannetv2_test.txt', 'r') as f:
+    with open(TESTING_FILES_TXT, 'r') as f:
       scene_names = f.readlines()
     scene_names = [s.split()[0] for s in scene_names]
+
+    for scene_name in scene_names:
+      scene_path = os.path.join(TESTING_SCANS_ROOT, '%s.ply' % scene_name)
+      coords, colors, pcd = load_file(scene_path)
+
+      with torch.no_grad():
+          voxel_size = 0.05
+          # Feed-forward pass and get the prediction
+          in_field = ME.TensorField(
+              features=normalize_color(torch.from_numpy(colors)),
+              coordinates=ME.utils.batched_coordinates([coords / voxel_size], dtype=torch.float32),
+              quantization_mode=ME.SparseTensorQuantizationMode.UNWEIGHTED_AVERAGE,
+              minkowski_algorithm=ME.MinkowskiAlgorithm.SPEED_OPTIMIZED,
+              device=device,
+          )
+          # Convert to a sparse tensor
+          sinput = in_field.sparse()
+          # Output sparse tensor
+          soutput = model(sinput)
+          # get the prediction on the input tensor field
+          out_field = soutput.slice(in_field)
+          logits = out_field.F
+
+      _, pred = logits.max(1)
+      pred = [VALID_CLASS_IDS_200[idx] for idx in pred.tolist()]
+      pred_res_list.append(pred)
+
     cwd = os.getcwd()
-    
     test_set_prediction_root = os.path.join(cwd, 'test_set_prediction')
     if not os.path.isdir(test_set_prediction_root):
       os.mkdir(test_set_prediction_root)
@@ -199,32 +180,99 @@ def test(curr_train_iter, model, data_loader, config, transform_data_fn=None, ha
       with open(os.path.join(test_set_prediction_root, result_txt), 'w') as f:
         for res in pred_res:
           f.write('%d\n' % res)
+    return
 
 
-  global_time = global_timer.toc(False)
+  # validation mode => do not output the prediction, no original point cloud is needed
+  else:
+    with torch.no_grad():
+      for iteration in range(max_iter):
+        data_timer.tic()
+        if config.return_transformation:
+          coords, input, target, pointcloud, transformation = data_iter.next()
+        else:
+          coords, input, target = data_iter.next()
+        data_time = data_timer.toc(False)
 
-  reordered_ious = dataset.reorder_result(ious)
-  reordered_ap_class = dataset.reorder_result(ap_class)
-  class_names = dataset.get_classnames()
-  wandb_log_dict = print_info(
-        iteration,
-        max_iter_unique,
-        data_time,
-        iter_time,
-        has_gt,
-        losses,
-        scores,
-        reordered_ious,
-        hist,
-        reordered_ap_class,
-        class_names=class_names,
-        data_type=data_type
-      )
-  
-  logging.info("Finished test. Elapsed time: {:.4f}".format(global_time))
+        # Preprocess input
+        iter_timer.tic()
 
-  # Explicit memory cleanup
-  if hasattr(data_iter, 'cleanup'):
-    data_iter.cleanup()
+        if config.normalize_color:
+          input[:, :3] = input[:, :3] / 255. - 0.5
+        sinput = ME.SparseTensor(input, coords, device=device) #.to(device)
 
-  return losses.avg, scores.avg, np.nanmean(ap_class), np.nanmean(per_class_iu(hist)) * 100, wandb_log_dict
+        # Feed forward
+        inputs = (sinput,)
+        soutput = model(*inputs)
+        output = soutput.F
+        
+        pred = get_prediction(dataset, output, target).int()
+        iter_time = iter_timer.toc(False)
+
+        if has_gt:
+          target_np = target.numpy()
+          num_sample = target_np.shape[0]
+          target = target.to(device)
+
+          cross_ent = criterion(output, target.long())
+          losses.update(float(cross_ent), num_sample)
+          scores.update(precision_at_one(pred, target), num_sample)
+          hist += fast_hist(pred.cpu().numpy().flatten(), target_np.flatten(), num_labels)
+          ious = per_class_iu(hist) * 100
+
+          prob = torch.nn.functional.softmax(output, dim=1)
+          ap = average_precision(prob.cpu().detach().numpy(), target_np)
+          aps = np.vstack((aps, ap))
+          # Due to heavy bias in class, there exists class with no test label at all
+          with warnings.catch_warnings():
+            warnings.simplefilter("ignore", category=RuntimeWarning)
+            ap_class = np.nanmean(aps, 0) * 100.
+
+        # if iteration == max_iter - 1 and iteration > 0:
+        #   reordered_ious = dataset.reorder_result(ious)
+        #   reordered_ap_class = dataset.reorder_result(ap_class)
+        #   class_names = dataset.get_classnames()
+        #   print_info(
+        #       iteration,
+        #       max_iter_unique,
+        #       data_time,
+        #       iter_time,
+        #       has_gt,
+        #       losses,
+        #       scores,
+        #       reordered_ious,
+        #       hist,
+        #       reordered_ap_class,
+        #       class_names=class_names,
+        #       data_type=data_type)
+        if iteration % config.empty_cache_freq == 0:
+          # Clear cache
+          torch.cuda.empty_cache()
+
+    global_time = global_timer.toc(False)
+
+    reordered_ious = dataset.reorder_result(ious)
+    reordered_ap_class = dataset.reorder_result(ap_class)
+    class_names = dataset.get_classnames()
+    wandb_log_dict = print_info(
+          iteration,
+          max_iter_unique,
+          data_time,
+          iter_time,
+          has_gt,
+          losses,
+          scores,
+          reordered_ious,
+          hist,
+          reordered_ap_class,
+          class_names=class_names,
+          data_type=data_type
+        )
+    
+    logging.info("Finished test. Elapsed time: {:.4f}".format(global_time))
+
+    # Explicit memory cleanup
+    if hasattr(data_iter, 'cleanup'):
+      data_iter.cleanup()
+
+    return losses.avg, scores.avg, np.nanmean(ap_class), np.nanmean(per_class_iu(hist)) * 100, wandb_log_dict
